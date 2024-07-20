d32ef1908556c6b30dc679fa1b14970ede13c29e
Nathally Souza <nathallyccd@gmail.com>
2024-07-20 17:02:14 -0300

    [test]update managerService.spec

--- src/resources/manager/service/manager.service.spec.ts
+++ src/resources/manager/service/manager.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ManagerService } from './manager.service';
+import { CustomerService } from '../../../resources/customer/service/customer.service';
+import { AccountsService } from '../../../resources/accounts/accounts.service';
 
 describe('ManagerService', () => {
   let service: ManagerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ManagerService],
+      providers: [ManagerService, CustomerService, AccountsService],
     }).compile();
 
     service = module.get<ManagerService>(ManagerService);
