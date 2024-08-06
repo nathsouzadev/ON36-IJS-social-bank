abc4cdfec42790b778d0db58889c3d739a1288c0
Nathally Souza <nathallyccd@gmail.com>
2024-08-06 00:13:08 -0300

    [TEST]create accounts.service tests

--- src/resources/accounts/accounts.service.spec.ts
+++ src/resources/accounts/accounts.service.spec.ts
@@ -12,7 +12,49 @@ describe('AccountsService', () => {
     service = module.get<AccountsService>(AccountsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be create account', () => {
+    const mockAccountDto = {
+      customerId: '0c2122f8-9d02-40d6-b84e-dbed3fb1f8a4',
+      customerIndex: 1,
+      balance: 1000,
+      type: 'current',
+    };
+
+    const response = service.create(mockAccountDto);
+    expect(response).toMatchObject({
+      account: {
+        id: expect.any(String),
+        type: 'current',
+        balance: 1000,
+        customerId: '0c2122f8-9d02-40d6-b84e-dbed3fb1f8a4',
+      },
+    });
+  });
+
+  it('should be update account', () => {
+    const mockUpdateAccountDto = {
+      accountId: 'ac8eede5-80d6-463a-8256-09c41dab5124',
+      customerId: '0c2122f8-9d02-40d6-b84e-dbed3fb1f8a4',
+      customerIndex: 1,
+      balance: 1000,
+      type: 'current',
+    };
+
+    const response = service.update(mockUpdateAccountDto);
+    expect(response).toMatchObject({
+      account: {
+        id: expect.any(String),
+        type: 'current',
+        balance: 1000,
+        customerId: '0c2122f8-9d02-40d6-b84e-dbed3fb1f8a4',
+      },
+    });
+  });
+
+  it('should be delete account', () => {
+    const response = service.delete('ac8eede5-80d6-463a-8256-09c41dab5124', 1);
+    expect(response).toMatchObject({
+      message: 'Account deleted successfully',
+    });
   });
 });
