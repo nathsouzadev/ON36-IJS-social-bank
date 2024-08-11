98fbab9c5dfda0c3323083a4f47af813e7a3d599
Nathally Souza <nathallyccd@gmail.com>
2024-08-11 07:15:35 -0300

    [feat]create cnpj wire in

--- src/resources/brasil/brasil.service.ts
+++ src/resources/brasil/brasil.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { CNPJResponse } from './models/cnpj-response.model';
+import { cnpjWireIn } from './adapters/cnpj-wire-in';
 
 @Injectable()
 export class BrasilService {
-  cnpj = async (cnpj: string) => {
+  cnpj = async (cnpj: string): Promise<CNPJResponse> => {
     try {
       const response = await axios({
         method: 'get',
         url: `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`,
       });
 
-      return {
-        cnpj: response.data.cnpj,
-        isValid: true,
-      };
+      return cnpjWireIn(response.status, cnpj);
     } catch (error) {
-      if (error.response.status === 404 || error.response.status === 400) {
-        return {
-          cnpj: cnpj,
-          isValid: false,
-        };
-      }
+      return cnpjWireIn(error.response.status, cnpj);
     }
   };
 }
